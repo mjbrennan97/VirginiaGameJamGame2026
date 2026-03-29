9aff646b838adc4ebc6072e2e9867f967802f495
mjbrennan97 <130258585+mjbrennan97@users.noreply.github.com>
2026-03-29 15:12:20 -0400

    key puzzle fully done

--- openingcasescript.gd
+++ openingcasescript.gd
@@ -7,4 +7,7 @@ func _ready():
 func _on_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			get_tree().change_scene_to_file("res://keypuzzle.tscn")
+			if InventoryManager.has_item("Key"):
+				get_tree().change_scene_to_file("res://keypuzzle.tscn")
+			else:
+				print("you need the item to pass")
